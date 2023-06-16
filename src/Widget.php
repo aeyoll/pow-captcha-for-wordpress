1353b3c7c7669c15e0e2e095c9a31737fb574b21
Jean-Philippe Bidegain <jp@bidega.in>
2023-06-16 16:37:22 +0200

    docs: add comments for the Widget class

--- src/Widget.php
+++ src/Widget.php
@@ -6,10 +6,19 @@ use Aeyoll\PowCaptchaForWordpress\Core;
 use GuzzleHttp\Client;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
+/**
+ * Class Widget
+ * Represents a widget for the POW Captcha plugin in WordPress.
+ */
 final class Widget
 {
     protected $client;
 
+    /**
+     * Retrieves the HTTP client for making API requests.
+     *
+     * @return Client The GuzzleHttp client instance.
+     */
     public function get_client()
     {
         if (!$this->client) {
@@ -29,6 +38,11 @@ final class Widget
         return $this->client;
     }
 
+    /**
+     * Loads the challenges from the API.
+     *
+     * @return array|null The challenges data as an associative array.
+     */
     public function load_challenges()
     {
         $response = $this->get_client()->post('GetChallenges?difficultyLevel=5');
@@ -38,30 +52,58 @@ final class Widget
         return $data;
     }
 
+    /**
+     * Retrieves a challenge from the cache or loads it from the API.
+     *
+     * @return string|null The challenge string.
+     */
     public function get_challenge()
     {
-        $cache_key = 'pow_captcha_for_wordpress_challenges';
-        $cache = new FilesystemAdapter('pow_captcha_for_wordpress', 24 * 3600);
 
+        // Define the cache key for storing the challenges
+        $cache_key = 'pow_captcha_for_wordpress_challenges';
+
+        // Create a new filesystem cache adapter, caching the values for a month
+        $cache = new FilesystemAdapter('pow_captcha_for_wordpress', 30 * 24 * 3600);
+
+        // Retrieve the challenges from the cache using the cache key
         $challengesCache = $cache->getItem($cache_key);
 
         if (!$challengesCache->isHit()) {
+            // If the challenges are not found in the cache, load them from the API
             $challenges = $this->load_challenges();
         } else {
+            // If the challenges are found in the cache, retrieve them
             $challenges = $challengesCache->get();
 
             if (count($challenges) < 5) {
+                // If the number of challenges is less than 5, reload them from the API
                 $challenges = $this->load_challenges();
             }
         }
 
+        // Get the first challenge from the array
         $challenge = array_shift($challenges);
+
+        // Update the challenges cache with the remaining challenges
         $challengesCache->set($challenges);
+
+        // Save the challenges cache
         $cache->save($challengesCache);
 
+        // Return the retrieved challenge
         return $challenge;
     }
 
+
+    /**
+     * Validates the CAPTCHA challenge and nonce.
+     *
+     * @param string $challenge The CAPTCHA challenge.
+     * @param string $nonce The nonce value.
+     *
+     * @return bool Indicates whether the CAPTCHA is valid or not.
+     */
     public function validate_captcha($challenge, $nonce): bool
     {
         $url = sprintf('Verify?challenge=%s&nonce=%s', $challenge, $nonce);
@@ -74,6 +116,9 @@ final class Widget
         }
     }
 
+    /**
+     * Enqueues the necessary scripts for the POW Captcha widget.
+     */
     public function pow_captcha_enqueue_widget_scripts()
     {
         $plugin = Core::$instance;
@@ -91,6 +136,11 @@ final class Widget
         );
     }
 
+    /**
+     * Generates the HTML placeholder for the POW Captcha widget.
+     *
+     * @return string The HTML code for the placeholder.
+     */
     public function pow_captcha_placeholder()
     {
         return <<<EOD
@@ -119,10 +169,17 @@ final class Widget
         EOD;
     }
 
-    public function pow_captcha_generate_widget_tag_from_plugin($plugin)
+    /**
+     * Generates the widget tag using the plugin's configuration.
+     *
+     * @param Core $plugin The plugin instance.
+     *
+     * @return string|null The generated widget tag.
+     */
+    public function pow_captcha_generate_widget_tag_from_plugin(Core $plugin)
     {
         if (!$plugin->is_configured()) {
-            return ;
+            return;
         }
 
         $api_url = $plugin->get_captcha_api_url();
@@ -131,7 +188,15 @@ final class Widget
         return $this->pow_captcha_generate_widget_tag($api_url, $challenge);
     }
 
-    public function pow_captcha_generate_widget_tag($api_url, $challenge)
+    /**
+     * Generates the widget tag using the provided API URL and challenge.
+     *
+     * @param string $api_url The API URL for the POW Captcha.
+     * @param string $challenge The CAPTCHA challenge.
+     *
+     * @return string The generated widget tag.
+     */
+    public function pow_captcha_generate_widget_tag(string $api_url, string $challenge)
     {
         $form = <<<EOD
         <style>

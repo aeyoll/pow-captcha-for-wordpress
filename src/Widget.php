826d8033a9031281b11f7e94fe7cd7fa4e798499
Jean-Philippe Bidegain <jp@bidega.in>
2025-07-30 12:03:10 +0200

    chore(version): bump version 1.0.10

--- src/Widget.php
+++ src/Widget.php
@@ -133,7 +133,7 @@ final class Widget
             'pow-captcha-css',
             plugin_dir_url(__FILE__) . '../assets/css/pow-captcha.css',
             array(),
-            '1.0.9'
+            '1.0.10'
         );
 
         // Enqueue JS
@@ -141,7 +141,7 @@ final class Widget
             'pow-captcha-lib-js',
             $plugin->get_captcha_api_url() . '/static/captcha.js',
             array(),
-            '1.0.9',
+            '1.0.10',
             true
         );
 
@@ -149,7 +149,7 @@ final class Widget
             'pow-captcha-js',
             plugin_dir_url(__FILE__) . '../assets/js/pow-captcha.js',
             array(),
-            '1.0.9',
+            '1.0.10',
             true
         );
     }

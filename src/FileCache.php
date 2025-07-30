77a6c86435bd822581d08f66d7c85b3e7c3d0ebe
Jean-Philippe Bidegain <jp@bidega.in>
2025-07-30 12:07:26 +0200

    refactor: use native function for atomic_file_put_contents

--- src/FileCache.php
+++ src/FileCache.php
@@ -127,12 +127,7 @@ class FileCache
             return false;
         }
 
-        global $wp_filesystem;
-        if (empty($wp_filesystem)) {
-            require_once(ABSPATH . '/wp-admin/includes/file.php');
-            WP_Filesystem();
-        }
-        return $wp_filesystem->move($tmpName, $filename, true);
+        return rename($tmpName, $filename);
     }
 
     /**

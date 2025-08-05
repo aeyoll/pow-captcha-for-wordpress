98c11762be80adccd5dd742fee8df11043a00121
Jean-Philippe Bidegain <jp@bidega.in>
2025-08-05 09:14:03 +0200

    fix: prevent direct access to ajax.php

--- ajax.php
+++ ajax.php
@@ -1,5 +1,10 @@
 <?php
 
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
 require_once __DIR__ . '/../../../wp-config.php';
 require_once __DIR__ . '/vendor/autoload.php';
 

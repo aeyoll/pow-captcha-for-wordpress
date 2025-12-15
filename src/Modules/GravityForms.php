5dd850cbdd25a3e8d66e70900a8d2f810537a5fb
Jean-Philippe Bidegain <jp@bidega.in>
2025-12-15 15:36:13 +0100

    refactor: prevent a deprecation warning on gravity form > 2.5

--- src/Modules/GravityForms.php
+++ src/Modules/GravityForms.php
@@ -4,6 +4,7 @@ namespace Aeyoll\PowCaptchaForWordpress\Modules;
 
 use Aeyoll\PowCaptchaForWordpress\Core;
 use Aeyoll\PowCaptchaForWordpress\Widget;
+use GFCommon;
 use GFFormsModel;
 use GGFormDisplay;
 
@@ -24,9 +25,23 @@ class GravityForms
         // Validate all form submissions
         add_filter('gform_validation', [$this, 'validate_form'], 10, 2);
 
-        // Add form settings
-        add_filter('gform_form_settings', [$this, 'add_form_settings'], 10, 2);
+        // Add form settings (gform_form_settings was deprecated in v2.5 and removed in v3.0)
+        if ($this->is_gravity_forms_version_at_least('2.5')) {
+            add_filter('gform_form_settings_fields', [$this, 'add_form_settings_fields'], 10, 2);
+        } else {
+            add_filter('gform_form_settings', [$this, 'add_form_settings_legacy'], 10, 2);
+        }
+
         add_filter('gform_pre_form_settings_save', [$this, 'save_form_settings']);
+    }
+
+    protected function is_gravity_forms_version_at_least(string $version): bool
+    {
+        if (! class_exists('GFCommon')) {
+            return false;
+        }
+
+        return version_compare(\GFCommon::$version, $version, '>=');
     }
 
     public function pow_captcha_enqueue_scripts($form, $ajax)
@@ -127,7 +142,41 @@ class GravityForms
         return $is_excluded;
     }
 
-    public function add_form_settings($settings, $form)
+    /**
+     * Add form settings fields for Gravity Forms 2.5+.
+     *
+     * @param array $fields The form settings fields.
+     * @param array $form   The form object.
+     *
+     * @return array
+     */
+    public function add_form_settings_fields(array $fields, array $form): array
+    {
+        $fields['pow_captcha_settings'] = [
+            'title' => esc_html__('POW Captcha', 'pow-captcha'),
+            'fields' => [
+                [
+                    'name' => 'pow_captcha_disable',
+                    'label' => esc_html__('Disable POW Captcha', 'pow-captcha'),
+                    'type' => 'toggle',
+                    'tooltip' => esc_html__('Disable POW Captcha for this form', 'pow-captcha'),
+                    'default_value' => false,
+                ],
+            ],
+        ];
+
+        return $fields;
+    }
+
+    /**
+     * Add form settings for Gravity Forms < 2.5 (legacy).
+     *
+     * @param array $settings The form settings.
+     * @param array $form     The form object.
+     *
+     * @return array
+     */
+    public function add_form_settings_legacy(array $settings, array $form): array
     {
         $checked = isset($form['pow_captcha_disable']) && $form['pow_captcha_disable'] ? 'checked="checked"' : '';
 
